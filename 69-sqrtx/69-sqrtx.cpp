e702ed1ed4f3679b9ac6fa36f42a453242bcacbf
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-05 23:54:41 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.51%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        //o(root n)
+        //return sqrt(x);
+        
+        
+        //O(logn)
+        if (x == 0) return 0;
+        int left = 1, right = x, ans;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                ans = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
     }
 };

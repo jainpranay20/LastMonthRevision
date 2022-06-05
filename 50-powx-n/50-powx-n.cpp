fe6a2111d0c3e66d3bf6b7bdfdeedf2d1a19b030
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-05 18:52:43 +0530

    Time: 8 ms (6.63%), Space: 5.9 MB (44.74%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double helper(double x, int n)
+    {
+        if(n==0)
+           return 1;
+        double temp=helper(x,n/2);
+        temp*=temp;
+        if(n%2==0)
+            return temp;
+        else return x*temp*1.0;
+    }
+    double myPow(double x, int n) {
+       if(n==0) return 1;
+        if(n>0) return helper(x,n);
+        else return 1.0/helper(x,n);
+    }
+};

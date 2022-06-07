ce1b51fbb3ce776b924d5246e1b329f6de2a4362
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-07 07:49:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (44.52%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,20 +1,50 @@
-class Solution {
-public:
-    //O(logn) approach
-    double helper(double x, int n)
+class Solution
+{
+    public:
+       	//O(logn) approach
+        double helper(double x, int n)
+        {
+            if (n == 0)
+                return 1;
+            double temp = helper(x, n / 2);
+            temp *= temp;
+            if (n % 2 == 0)
+                return temp;
+            else return x *temp *1.0;
+        }
+    double myPow(double x, int n)
     {
-        if(n==0)
-           return 1;
-        double temp=helper(x,n/2);
-        temp*=temp;
-        if(n%2==0)
-            return temp;
-        else return x*temp*1.0;
+        if (n == 0) return 1;
+        if (n > 0) return helper(x, n);
+        else return 1.0 / helper(x, n);
     }
-    double myPow(double x, int n) {
-       if(n==0) return 1;
-        if(n>0) return helper(x,n);
-        else return 1.0/helper(x,n);
-    }
+
+   	//  TLE
+   	//             if(n>=0){
+   	//                 double ans = 1;
+   	//                 for (int i = 0; i < n; i++)
+   	//                     ans = ans * x;
+   	//             return ans;
+   	//             }
+   	//             else 
+   	//             {
+   	//                 double ans=1;
+   	//                 for (int i = 0; i < abs(n); i++)
+   	//                 ans=(1/x)*ans;
+   	//                 return ans;
+   	//             }
+   	//         }
+
+   	//  TLE
+   	//             double myPow(double x, int n)
+   	//         {
+
+   	// if (n == 0)
+   	//     return 1;
+   	// if (n < 0)
+   	// {
+   	//     n = -n;
+   	//     x = 1 / x;
+   	// }
+   	// return (n % 2 == 0) ? pow(x *x, n / 2) : x* pow(x *x, n / 2);
 };
- 

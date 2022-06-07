f440ca79e5a05145eee1721f6b8694cb9c7ad476
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-07 18:02:11 +0530

    Time: 492 ms (14.39%), Space: 49.4 MB (13.56%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();  
+        sort(nums.begin() , nums.end());  // sort the array to use the two pointers method
+        vector<vector<int>> ans;  
+        set<vector<int>> store;   // to store and remove the duplicate answers
+		
+        for(int i = 0 ; i < n; i++){
+		     for(int j = i + 1; j < n ; j++){
+                int new_target  =  target - nums[i] - nums[j];
+                int x = j+1 , y = n-1;
+                while(x < y){
+                    int sum = nums[x] + nums[y];
+                    if(sum > new_target) y--;
+                    else if(sum < new_target ) x++;
+                    else  {
+                        store.insert({nums[i] , nums[j] , nums[x] , nums[y]});
+                        x++;
+                        y--;
+                    };
+                }
+            }
+        }
+        for(auto i : store){
+            ans.push_back(i);  // store the answers in an array(ans)
+        }
+        return ans;
+    }
+        
+        
+    //  TLE
+    //     vector<vector<int>> ans;
+    //     vector<int> a;
+    //     int n=nums.size();
+    //     sort(nums.begin(),nums.end());
+    //     set<vector<int>> store;
+    //     for(int i=0;i<n-3;i++)
+    //     {
+    //         for(int j=i+1;j<n-2;j++)
+    //         {
+    //             int k=j+1,l=n-1;
+    //             int new_target = target - nums[i] - nums[j];
+    //             while(k<l)
+    //             {
+    //                 if(nums[k]+nums[l]==target)
+    //                 {
+    //                    store.insert({nums[i] , nums[j] , nums[k] , nums[l]});
+    //                 }
+    //                 else if(nums[k]+nums[l]<target)
+    //                 {
+    //                     k++;
+    //                 }
+    //                 else if(nums[k]+nums[l]>target)
+    //                 {
+    //                     l--;
+    //                 }
+    //             }
+    //             ans.push_back(a);
+    //         }
+    //     }
+    //     for(auto i : store){
+    //         ans.push_back(i);  // store the answers in an array(ans)
+    //     }
+    //     return ans;
+    // }
+};

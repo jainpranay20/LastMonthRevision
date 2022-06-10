54f0da5babfad003fc26d13842561530abea8181
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-10 11:44:07 +0530

    Time: 21 ms (7.40%), Space: 15.1 MB (8.40%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // using dummy node TC O( N1 + N2 ) SPACE O( N1 + N2)
+        ListNode* temp1=list1, *temp2=list2;
+        ListNode* list3=new ListNode();
+        ListNode* temp3 = list3;
+        
+        while(temp1 && temp2)
+        {
+            if(temp1->val <= temp2->val)
+            {
+                temp3->next=new ListNode(temp1->val);
+                temp1=temp1->next;
+                temp3=temp3->next;
+            }    
+            else
+            {
+                temp3->next=new ListNode(temp2->val);
+                temp2=temp2->next;
+                temp3=temp3->next;
+            }
+        }
+        
+        while(temp1)
+        {
+            temp3->next=new ListNode(temp1->val);
+            temp1=temp1->next;
+            temp3=temp3->next;
+        }
+        
+        while(temp2)
+        {
+            temp3->next=new ListNode(temp2->val);
+            temp2=temp2->next;
+            temp3=temp3->next;
+        }
+        
+        return list3->next;
+        
+        
+        
+        
+        // it will just connect 2 linked list 1 with second not correct approach
+        // ListNode* temp=list1;
+        // while(temp!=NULL and temp->next!=NULL)
+        // {
+        //     temp=temp->next;
+        // }
+        // temp->next=list2;
+        // return list1; 
+        
+        
+        
+    }
+};

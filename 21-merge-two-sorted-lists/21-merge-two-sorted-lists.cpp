05f3910706d7f889ab6568f871f1e8e81a2e546d
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-10 16:37:56 +0530

    Time: 17 ms (18.79%), Space: 14.8 MB (43.32%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -12,23 +12,62 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         // RECURSIVE
-        if(list1 == NULL){
-			return list2;
-		}
-		if(list2 == NULL){
-			return list1;
-		}        
-		if(list1->val < list2->val){
-			list1->next = mergeTwoLists(list1->next, list2);
-			return list1;
-		}
-		else{
-			list2->next = mergeTwoLists(list1, list2->next);
-			return list2;            
-		}
+		// if(list1 == NULL){
+		// 	return list2;
+		// }
+		// if(list2 == NULL){
+		// 	return list1;
+		// }        
+		// if(list1->val < list2->val){
+		// 	list1->next = mergeTwoLists(list1->next, list2);
+		// 	return list1;
+		// }
+		// else{
+		// 	list2->next = mergeTwoLists(list1, list2->next);
+		// 	return list2;            
+		// }
         
         
         
+        //ITERATIVE WITHOUT SPACE
+        if(list1==nullptr)
+            return list2;
+        if(list2==nullptr)
+            return list1;
+        ListNode *ptr;
+        if(list1->val < list2-> val)
+        {
+            ptr=list1;
+            list1=list1->next;  
+        }
+        else{
+            ptr=list2;
+            list2=list2->next;
+        }
+        ListNode *curr=ptr;
+        while(list1 && list2)
+        {
+            if(list1->val > list2->val)
+            {
+                curr->next=list2;
+                list2=list2->next;
+            }
+            else{
+                curr->next=list1;
+                list1=list1->next;
+            }
+            curr=curr->next;
+        }
+        if(!list1)
+            curr->next=list2;
+        else
+            curr->next=list1;
+            
+        return ptr;
+        
+        
+        
+        // ITERATIVE APPROACH INPLACE
         // using dummy node TC O( N1 + N2 ) SPACE O( N1 + N2)
         // brute force approach using an extra linklist 
         // like we use to do in sorted arrays

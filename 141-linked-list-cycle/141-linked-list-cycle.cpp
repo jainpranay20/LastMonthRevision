85accd3d465cf982bd6afff57517e49b44f0a088
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-11 17:16:15 +0530

    Time: 34 ms (6.06%), Space: 10.6 MB (10.14%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -1,26 +1,52 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode(int x) : val(x), next(NULL) {}
+ *};
  */
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        ListNode *slowptr=head;
-        if(slowptr==NULL || slowptr->next==NULL)
+class Solution{
+    public:
+        bool hasCycle(ListNode *head){
+            // o(n) time complexity O(n) space
+            unordered_set<ListNode*> st;
+            while (head != NULL)
+            {
+                if (st.find(head) != st.end()) return true;
+                st.insert(head);
+                head = head->next;
+            }
             return false;
-        ListNode *fastptr=head->next;
-        while(slowptr!=fastptr && fastptr!=NULL && fastptr->next!=NULL)
-        {
-            slowptr=slowptr->next;
-            fastptr=fastptr->next->next;
+            
+            // optimised approach
+            // if(head == NULL)
+            // return false;
+            // ListNode *fast = head;
+            // ListNode *slow = head;
+            // while(fast != NULL && fast ->next != NULL)
+            // {
+            //     fast = fast->next->next;
+            //     slow = slow->next;
+            //     if(fast == slow)
+            //         return true;
+            // }
+            // return false;
+            
+
+           	// unoptimised code
+            // ListNode *slowptr = head;
+            // if (slowptr == NULL || slowptr->next == NULL)
+            //     return false;
+            // ListNode *fastptr = head->next;
+            // while (slowptr != fastptr && fastptr != NULL && fastptr->next != NULL)
+            // {
+            //     slowptr = slowptr->next;
+            //     fastptr = fastptr->next->next;
+            // }
+            // if (slowptr == fastptr)
+            //     return true;
+            // else
+            //     return false;
         }
-        if(slowptr==fastptr)
-            return true;
-        else
-            return false;
-    }
 };

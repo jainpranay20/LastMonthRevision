a6e4d736359174ead73896caa4d39d24475e6594
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-13 12:02:35 +0530

    Time: 13 ms (22.10%), Space: 8.6 MB (91.19%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+    public:
+        int minimumTotal(vector<vector < int>> &triangle)
+        {
+                for (int level = size(triangle) - 2; level >= 0; level--)	
+                    // start from bottom-1 level
+                    for (int i = 0; i <= level; i++)
+        // for every cell: we could have moved here from same index or index+1 of next level
+                        triangle[level][i] += min(triangle[level + 1][i], triangle[level + 1][i + 1]);
+                return triangle[0][0];	
+                // lastly t[0][0] will contain accumulated sum of minimum path
+            }
+};

44175017e73a74d8624efc143142f57afcbe3824
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-13 12:42:53 +0530

    Time: 16 ms (11.39%), Space: 8.7 MB (70.75%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,16 +1,13 @@
-class Solution
-{
-    public:
-        int minimumTotal(vector<vector < int>> &triangle)
-        {
-            // bhaut sahi question h y
-            //Top-Down Dynamic Programming
-                for (int level = size(triangle) - 2; level >= 0; level--)	
-                    // start from bottom-1 level
-                    for (int i = 0; i <= level; i++)
-        // for every cell: we could have moved here from same index or index+1 of next level
-                        triangle[level][i] += min(triangle[level + 1][i], triangle[level + 1][i + 1]);
-                return triangle[0][0];	
-                // lastly t[0][0] will contain accumulated sum of minimum path
-            }
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+    for(int i=n-2;i>=0;i--)
+    {
+        for(int j=0;j<=i;j++){
+            triangle[i][j]=triangle[i][j]+min(triangle[i+1][j],triangle[i+1][j+1]);
+        }
+    }
+        return triangle[0][0];
+    }
 };

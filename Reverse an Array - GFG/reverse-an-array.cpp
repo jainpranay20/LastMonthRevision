09d0598f713a75ddb67742ffe9d8461239a3268d
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-23 14:06:01 +0530

    Added solution - LeetHub

--- Reverse an Array - GFG/reverse-an-array.cpp
+++ Reverse an Array - GFG/reverse-an-array.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void reverse(vector<int> &v, int n){
    int low =0;
    int high = n-1;
-   while(low<=high){
+   while(low<high){
        swap(v[low], v[high]);
        low++;
        high--;
@@ -18,7 +18,8 @@ while(t--){
     cin>>n;
     vector<int>v (n);
     for(int i =0; i<n;i++) cin>>v[i];
-    reverse(v.begin(),v.end());
+    //reverse(v.begin(),v.end());
+    reverse(v,n);
     for(auto it1 : v) cout<<it1<<" ";
     cout<<endl;
 }

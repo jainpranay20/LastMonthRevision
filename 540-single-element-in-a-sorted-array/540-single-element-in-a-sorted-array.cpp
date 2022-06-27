592b04efd8e6716381ad5eb200b52241be2193a1
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-06-27 14:38:48 +0530

    Time: 33 ms (73.41%), Space: 22.5 MB (8.77%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int low=0,high=nums.size()-1;
         if(nums.size()==1)
             return nums[0];
-        int l = 0,r=nums.size()-1;
-        while(l<=r){
-            int m = l + (r-l)/2;
-            if(m%2){
-				// (even odd) i.e. we are on the left side of unique element--> go right
-                if(nums[m-1]==nums[m])
-                    l = m+1;
-				// (even odd) pair violated, happens on the right side of unique element--> go left
-                else{
-                    r = m-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(mid%2==0)
+            {
+                if(nums[mid]==nums[mid+1])
+                {
+                    low=mid+1;
                 }
-            }else{
-				// (even odd) i.e. we are on the left side of unique element--> go right
-                if(nums[m]==nums[m+1]){
-                    l = m+1;
-				// (even odd) pair violated, happens on the right side of unique element--> go left
-                }else{
-                    r = m-1;
+                else{
+                    high=mid-1;
+                }
+            }
+            else
+            {
+                if(nums[mid]==nums[mid-1])
+                {
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
                 }
             }
         }
-        return nums[l];
+        return nums[low];
     }
 };

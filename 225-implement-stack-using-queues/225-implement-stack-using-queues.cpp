3c012bdda91a0a84cc0fb7f883e58289359df164
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-07-01 21:28:51 +0530

    Time: 0 ms (100.00%), Space: 7 MB (17.44%) - LeetHub

---
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -0,0 +1,49 @@
+class MyStack {
+public:
+    queue<int> q2;
+    queue<int> q1;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+       q2.push(x);
+        while(!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        while(!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+    }
+   /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int x = q1.front();
+        q1.pop();
+        return x;
+    }
+    
+    /** Get the top element. */
+    int top() {
+         return q1.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        if(q1.empty())
+            return true;
+        return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */

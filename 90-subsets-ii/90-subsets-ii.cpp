219ddc5e31e9133412a6cca9bc34a88f606ba257
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-07-16 20:55:42 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (96.70%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,45 +1,26 @@
 class Solution {
 public:
-//      vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-//         vector<vector<int>> ans;       //to store all susbets
-//         vector<int> curr;         //to store current individual subset (that we will build)
-//         sort(nums.begin(),nums.end());      //sort the array so that duplicates are adjacent 
-//         helper(nums,ans,curr,0);       //we start from index 0
-//         return ans;
-        
-//     }
-    
-//     void helper(vector<int>& nums, vector<vector<int>>& ans, vector<int>& curr, int idx){
-//         ans.push_back(curr);       //we include current susbet into final ans
-//         for(int i=idx;i<nums.size();i++){     //check for all possibilites
-//             if(i>idx &&  nums[i]==nums[i-1]) continue;      //if duplicate then we continue
-//             curr.push_back(nums[i]);     //we include nums[i] in current subset
-//             helper(nums,ans,curr,i+1); 
-//             curr.pop_back();         //to get subset without nums[i]
-//         }
-//     }  
-// };
-void solve(vector<int> &arr, int N,int index,vector<vector<int>> &all,vector<int> &ans,set<vector<int>> &res)
+    void solve(vector<vector<int>> &all,vector<int> &ans,vector<int> &nums,int index,int N)
     {
-        if(index==N)
+        if(index==nums.size())
         {
-            res.insert(ans);
-            return;
+            all.push_back(ans);
+            return ;
         }
-        ans.push_back(arr[index]);
-        solve(arr,N,index+1,all,ans,res);
+        ans.push_back(nums[index]);
+        solve(all,ans,nums,index+1,N);
         ans.pop_back();
-        solve(arr,N,index+1,all,ans,res);
+        index++;
+        while(index<=nums.size()-1 && nums[index]==nums[index-1])
+            index++;
+        solve(all,ans,nums,index,N);
     }
-    vector<vector<int>> subsetsWithDup(vector<int>& arr) {
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> all;
         vector<int> ans;
-        set<vector<int>> res;
-        int N=arr.size();
-        sort(arr.begin(),arr.end());
-        solve(arr,N,0,all,ans,res);
-        for(auto i:res)
-            all.push_back(i);
-
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        solve(all,ans,nums,0,n);
         return all;
-    }};
+    }
+};

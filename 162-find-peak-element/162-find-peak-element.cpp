f5e9bf8ef4b1e5c906da58864dcad3bd89a15762
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-07-21 14:17:08 +0530

    Time: 3 ms (91.49%), Space: 8.9 MB (49.64%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-    //     int low=0;
-    //     int n=nums.size()-1;
-    //     int high=n;
-    //     while(low<=high)
-    //     {
-    //         int mid=(low+high)/2;
-    //         if((mid-1<0 ||nums[mid]>=nums[mid-1]) && (mid+1>=n || nums[mid]>=nums[mid+1]))
-    //             return mid;
-    //         else if(nums[mid]>nums[mid+1])
-    //             high=mid-1;
-    //         else
-    //             low=mid+1;
-    //     }
-    //     return -1;
-    // }
-         int n = nums.size();
-        int s=0;
-        int e = n-1;
-        while(s<=e)
+        int low=0;
+        int n=nums.size();
+        int high=n-1;
+        while(low<=high)
         {
-            int m = s +(e-s)/2;
-            if((m-1<0 || nums[m] >= nums[m-1]) && (m+1>=n || nums[m]>=nums[m+1]))
-                return m;
-            else if(nums[m+1] > nums[m])
-                s =m+1;
-            else 
-                e = m-1;
+            int mid=(low+high)/2;
+            if((mid-1<0 ||nums[mid]>=nums[mid-1]) && (mid+1>=n || nums[mid]>=nums[mid+1]))
+                return mid;
+            else if(nums[mid+1]>nums[mid])
+                low=mid+1;
+            else
+                high=mid-1;
         }
         return -1;
     }
+        //  int n = nums.size();
+        // int s=0;
+        // int e = n-1;
+        // while(s<=e)
+        // {
+        //     int m = s +(e-s)/2;
+        //     if((m-1<0 || nums[m] >= nums[m-1]) && (m+1>=n || nums[m]>=nums[m+1]))
+        //         return m;
+        //     else if(nums[m+1] > nums[m])
+        //         s =m+1;
+        //     else 
+        //         e = m-1;
+        // }
+        // return -1;
+    
 };

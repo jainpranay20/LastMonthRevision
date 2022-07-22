53409cdb527b90130fc8ffb3df7248d004696d5c
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-07-22 06:06:32 +0530

    Time: 3 ms (91.49%), Space: 10.6 MB (49.64%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
+    int findPeakE(vector<int>nums, int low, int high) {
+    if (low == high)
+        return low;
+    int mid = (low + high) / 2;
+    if (nums[mid] > nums[mid + 1])
+        return findPeakE(nums, low, mid);
+    return findPeakE(nums, mid + 1, high);
+}
+
     int findPeakElement(vector<int>& nums) {
-        int low=0;
-        int n=nums.size();
-        int high=n-1;
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            if((mid-1<0 ||nums[mid]>=nums[mid-1]) && (mid+1>=n || nums[mid]>=nums[mid+1]))
-                return mid;
-            else if(nums[mid+1]>nums[mid])
-                low=mid+1;
-            else
-                high=mid-1;
-        }
-        return -1;
+        int a=findPeakE(nums,0,nums.size()-1);
+        return a;
     }
-        //  int n = nums.size();
-        // int s=0;
-        // int e = n-1;
-        // while(s<=e)
-        // {
-        //     int m = s +(e-s)/2;
-        //     if((m-1<0 || nums[m] >= nums[m-1]) && (m+1>=n || nums[m]>=nums[m+1]))
-        //         return m;
-        //     else if(nums[m+1] > nums[m])
-        //         s =m+1;
-        //     else 
-        //         e = m-1;
-        // }
-        // return -1;
-    
+        
+    //     int low=0;
+    //     int n=nums.size();
+    //     int high=n-1;
+    //     while(low<=high)
+    //     {
+    //         int mid=(low+high)/2;
+    //         if((mid-1<0 ||nums[mid]>=nums[mid-1]) && (mid+1>=n || nums[mid]>=nums[mid+1]))
+    //             return mid;
+    //         else if(nums[mid+1]>nums[mid])
+    //             low=mid+1;
+    //         else
+    //             high=mid-1;
+    //     }
+    //     return -1;
+    // }
 };

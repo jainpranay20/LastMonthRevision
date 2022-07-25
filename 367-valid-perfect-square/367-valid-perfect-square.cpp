f0487a11dad30bcbfb7daba3ee92c5823370de3d
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-07-25 21:40:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (64.45%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -7,4 +7,19 @@ public:
         else
             return false;
     }
+//         if(num==0)return 0;
+//         int low=1,high=num,mid;
+//         while(low<=high)
+//         {
+                  
+//             mid=(low+high)/2;
+//             if(mid*mid==num)
+//                 return true;
+//             if(mid<num/mid)
+//                  low=mid+1;
+//             else if(mid>num/mid)
+//                high=mid-1; 
+//         }
+//         return false;
+//     }
 };

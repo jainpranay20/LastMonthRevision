67f87d77b0d7559121868fa25e16e5df3840cfa3
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-08-12 23:26:25 +0530

    Time: 12 ms (30.08%), Space: 13.4 MB (58.14%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,34 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-        class Solution {
-private: 
-   map<int, map<int, multiset<int>>> result;
-   
-   void inorderTraversal(TreeNode* node, int hIndex, int vIndex) {
-       if (node == NULL) return;
-       
-       inorderTraversal(node->left, hIndex-1, vIndex+1);
-       result[hIndex][vIndex].insert(node->val);
-       inorderTraversal(node->right, hIndex+1, vIndex+1);
-   }
-
+class Solution {
 public:
-   vector<vector<int>> verticalTraversal(TreeNode* root) {
-       inorderTraversal(root, 0, 0);
-       
-       vector<vector<int>> resultVector;
-       for(auto col: result) {
-           vector<int> colVector;
-           for(auto node: col.second) {
-               for(auto val: node.second) {
-                   colVector.push_back(val);   
-               }
-           }
-           resultVector.push_back(colVector);
-       }
-       return resultVector;
-   }
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+	   //multiset is used to keep the values in ascending order and we can have repetitive values too
+        map<int, map<int, multiset<int>>>mp; 
+        queue<pair<TreeNode*, pair<int, int>>>q;
+		
+        q.push({root, {0, 0}});          //coordinates of the root are (0,0)
+        while(!q.empty()){
+            TreeNode* node = q.front().first;
+            int x = q.front().second.first, y = q.front().second.second;
+            q.pop();
+            mp[x][y].insert(node->val);  //inserting values in multiset corresponding to their coordinates 
+            if(node->left) q.push({node->left, {x-1, y+1}});
+            if(node->right) q.push({node->right, {x+1, y+1}});
+        }
+		
+        vector<vector<int>>ans;
+        for(auto p : mp){                //traversing the map
+            vector<int>level;
+            for(auto s : p.second){      //p.second is map<int, multiset<int>>
+			    //inserting all the values of the multiset in level vector
+                level.insert(level.end(), s.second.begin(), s.second.end()); 
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
 };
-   

84360cf6c83c53bb39026b57a1ce8d06c70e9e2a
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-08-14 17:48:43 +0530

    Time: 28 ms (39.27%), Space: 11.5 MB (29.96%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -11,59 +11,35 @@
 class Solution {
 public:
    // ListNode* reverseKGroup(ListNode* head, int k) {
-        int lengthofll(ListNode* head){
-        int cnt=0;
-        while(head != nullptr){
-            cnt++;
-            head = head->next;
+        ListNode *reverse(ListNode* head,int k,int count1)
+    {
+        if(count1<k)
+            return head;
+        ListNode *curr=head;int count=0;
+        ListNode *prev=nullptr,*nxt=nullptr;
+        while(curr!=nullptr and count<k)
+        {
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+            count++;
         }
-        return cnt;
+        if(nxt!=nullptr)
+        head->next=reverse(curr,k,count1-k);
+        return prev;
+    }
+        
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode *curr=head;
+        int count1=0;
+        while(curr!=NULL)
+        {
+            count1++;
+            curr=curr->next;
+        }
+        curr=head;
+        return reverse(head,k,count1);
     }
     
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        if(!head || !head->next || k == 1) return head; 
-        int n = lengthofll(head);
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        ListNode* pre = dummy;
-        ListNode* curr;
-        ListNode* nex;
-        while(n >= k){
-            curr = pre->next;
-            nex = curr->next;
-            for(int i=1;i < k;i++){
-                curr->next = nex->next;
-                nex->next = pre->next;
-                pre->next = nex;
-                nex = curr->next;
-            }
-            pre = curr;
-            n = n-k;
-        }
-        return dummy->next;
-    }
-        // time complexity O(n) auxiliary space o(n/k)
-    //     ListNode* cursor = head;
-    //     for(int i = 0; i < k; i++){
-    //         if(cursor == nullptr) return head;
-    //         cursor = cursor->next;
-    //     }
-    //     if(!head)
-    //         return nullptr;
-    //     int count=0;
-    //     ListNode *curr=head;
-    //     ListNode *prev=nullptr;
-    //     ListNode *next=NULL;
-    //     while(curr!=NULL && count<k)
-    //     {
-    //         next=curr->next;
-    //         curr->next=prev;
-    //         prev=curr;
-    //         curr=next;
-    //         count++;
-    //     }
-    //     ListNode *cp=next;
-    //     if(next!=nullptr) head->next=reverseKGroup(next,k);
-    //     return prev;
-    // }
 };

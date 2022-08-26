48fcf915bab6d680d0c5dfa2aeca6a316186445a
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-08-26 15:59:20 +0530

    Time: 63 ms (14.69%), Space: 6 MB (68.93%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,20 +1,22 @@
-class Solution {
-public:
-    bool reorderedPowerOf2(int n) {
-        // if((n&(n-1))==0)
-        // {
-        //     return true;
-        // }
-        // else
-        //     return false;
-        
-        string x=to_string(n);
-        sort(x.begin(),x.end());
-         do {
-                if(x[0]=='0') continue;
-                int n1=stoi(x);
-                if(n1 && !(n1&(n1-1))) return true;
-            } while (next_permutation(x.begin(),x.end()));
-        return false;
-    }
+class Solution
+{
+    public:
+        bool reorderedPowerOf2(int n)
+        {
+            string x = to_string(n);
+            sort(x.begin(), x.end());
+
+            if (x[0] != '0')
+            {
+                int n1 = stoi(x);
+                if (n1 && !(n1 &(n1 - 1))) return true;
+            }
+            while (next_permutation(x.begin(), x.end()))
+            {
+                if (x[0] == '0') continue;
+                int n1 = stoi(x);
+                if (n1 && !(n1 &(n1 - 1))) return true;
+            }
+            return false;
+        }
 };

28f71ee9a1b29331f1d0b8561e5920277ea4b643
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-08-28 18:27:41 +0530

    Time: 12 ms (85.56%), Space: 10.5 MB (67.28%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,19 +1,40 @@
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        int res=1;
-        vector<int> ans(nums.size(),1);
-        for(int i=0;i<nums.size();i++)
+class Solution
+{
+    public:
+        int lengthOfLIS(vector<int> &nums)
         {
-            for(int j=0;j<i;j++)
+
+            if (nums.size() == 0) return 0;
+            vector<int> v;
+            v.push_back(nums[0]);
+            for (int i = 1; i < nums.size(); i++)
             {
-                if(nums[i]>nums[j])
+                if (nums[i] > v.back())
                 {
-                    ans[i]=max(ans[i],ans[j]+1);
+                    v.push_back(nums[i]);
+                }
+                else
+                {
+                    int idx = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
+                    v[idx] = nums[i];
                 }
             }
-            res=max(res,ans[i]);
+            return v.size();
+
+           	// O(n2) onr of the best solution
+           	// int res=1;
+           	// vector<int> ans(nums.size(),1);
+           	// for(int i=0;i < nums.size();i++)
+           	// {
+           	//     for(int j=0;j < i;j++)
+           	//     {
+           	//         if(nums[i]>nums[j])
+           	//         {
+           	//             ans[i]=max(ans[i],ans[j]+1);
+           	//         }
+           	//     }
+           	//     res=max(res,ans[i]);
+           	// }
+           	// return res;
         }
-        return res;
-    }
 };

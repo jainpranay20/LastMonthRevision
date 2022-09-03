965e5cb60d5ae902766aaed5efa40eb0172812b5
Pranay Jain <82707594+jainpranay20@users.noreply.github.com>
2022-09-04 00:26:33 +0530

    Time: 12 ms (36.60%), Space: 9.9 MB (93.51%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int first = INT_MIN,flag=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(first<nums[i])
-            {
-                first=nums[i];
-                flag=i;
+        int a = 0, b = 0;
+        for (int i = 0;i < nums.size();i++) {
+            if (nums[i] > a) {
+                b = a;  // delegate previous max value to 2nd max value
+                a = nums[i];  // set new max value
+            } else if (nums[i] > b) {
+                b = nums[i];
             }
         }
-        cout<<first;
-        int second=INT_MIN;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(second<nums[i] and i!=flag)
-            {
-                second=nums[i];
-            }
-        }
-        cout<<second;
-        return (first-1)*(second-1);
+        return (a-1)*(b-1);
     }
 };
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         int first = INT_MIN,flag=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(first<nums[i])
+//             {
+//                 first=nums[i];
+//                 flag=i;
+//             }
+//         }
+//         cout<<first;
+//         int second=INT_MIN;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(second<nums[i] and i!=flag)
+//             {
+//                 second=nums[i];
+//             }
+//         }
+//         cout<<second;
+//         return (first-1)*(second-1);
+//     }
+// };
